fa169032f35cc573a3aacd2174f0b373ad4494c8
Veikka Puhalainen <veikka.puhalainen@gmail.com>
2024-04-11 17:25:24 +0300

    Added function writeRentToFile which writes every created rent to a file jsonFileRent.txt

--- src/main/scala/Classes/Rent.scala
+++ src/main/scala/Classes/Rent.scala
@@ -1,9 +1,9 @@
 package Classes
-import java.time.LocalDate
+import java.time.{LocalDate, Period}
 import scala.collection.mutable
 
-class Rent(notification: Notification, whoRented: User, durationDays: Int, durationHours: Int, price: Double, returned: Boolean):
-
+case class Rent(notification: Notification, whoRented: User, amount: Int, startDay: LocalDate, endDay: LocalDate, startHour: Int, endHour: Int, price: Double):
+  
   val comments = mutable.Buffer[String]()
 
   def addComment(comment: String): Unit = comments += comment
@@ -14,4 +14,4 @@ class Rent(notification: Notification, whoRented: User, durationDays: Int, durat
   def cancelRent(): Unit = 
     this.whoRented.rents -= this
 
-  override def toString = s"Rented: ${notification.name}, who rented: $whoRented, durationDays: $durationDays, durationHours: $durationHours, price: $price, returned: $returned"
+  override def toString = s"Rented: ${notification.name}, who rented: $whoRented, amount: $amount, start: d:$startDay, h:$startHour, d:end: $endDay, h:$endHour, price: $price"
