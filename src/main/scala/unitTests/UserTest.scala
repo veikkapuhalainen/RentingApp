d24fdd01007493fde10ed69fb815d1dfab82e2aa
Veikka Puhalainen <veikka.puhalainen@gmail.com>
2024-04-12 16:09:22 +0300

    User can now create packages of products which he can rent.

--- src/main/scala/unitTests/UserTest.scala
+++ src/main/scala/unitTests/UserTest.scala
@@ -3,16 +3,21 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import Classes.Notification
-import Classes.User
-import Classes.Category
-import scala.collection.mutable
+import Classes.*
+import java.time.Period
+import java.time.LocalDate
 
 class UserTest extends AnyFlatSpec with Matchers:
- "makeNotification" should "behave as expected when making a new notification" in {
-    //val User1 = User()
-   // val categ = Category("sport")
-    val notif = Notification("bike", User1, 10.0, 5.0, "new bike", categ)
-   // User1.makeNotification("bike", 10.0, 5.0, "new bike", categ) shouldEqual(notif)
+ "making rent" should "behave as expected when making a new rent" in {
+    val User1 = User("veikka", "osoite", "7787798")
+    val User2 = User("veijo", "osoite", "7787798")
+    val categ = Category("sport")
+    val notif = Notification("testi", User1, 3.0, 4.0, "hyvä tuote", Category("sport"), 1,true)
+    val rent = Rent(notif, User2, 1, LocalDate.now(), LocalDate.now(), 0, 0, notif.pricePerDay )
+    notif.reservedDates += Period.between(LocalDate.now(), LocalDate.now() )
+    notif.reservedDates.head shouldEqual(Period.between(LocalDate.now(), LocalDate.now() ))
+    //User1.makeNotification("testi", 3.0, 4.0, "hyvä tuote", Category("sport"), 1,true) shouldEqual(notif)
+
  }
-*/
+ 
+ */
