3546528ed8f3b6286aaab061ed3a8ff764d8b3e3
Veikka Puhalainen <veikka.puhalainen@gmail.com>
2024-04-13 21:30:19 +0300

    Added datePicker for each notification that shows reserved days as red and shows rent starting and ending times

--- build.sbt
+++ build.sbt
@@ -9,11 +9,19 @@ lazy val root = (project in file("."))
 libraryDependencies += "org.scalafx" % "scalafx_3" % "20.0.0-R31"
 libraryDependencies += "org.scalatest" %% "scalatest-flatspec" % "3.2.15"
 libraryDependencies += "org.scalatest" %% "scalatest-shouldmatchers" % "3.2.15"
+libraryDependencies ++= Seq(
+  "org.openjfx" % "javafx-controls" % "19.0.2",
+  "org.openjfx" % "javafx-fxml" % "19.0.2",
+  "org.openjfx" % "javafx-web" % "19.0.2",
+  "org.openjfx" % "javafx-media" % "19.0.2",
+  "org.openjfx" % "javafx-swing" % "19.0.2",
+  "org.openjfx" % "javafx-base" % "19.0.2"
+)
+libraryDependencies += "com.calendarfx" % "view" % "11.12.7"
 
-val circeVersion = "0.14.3"
 
 libraryDependencies ++= Seq(
-  "io.circe" %% "circe-core",
-  "io.circe" %% "circe-generic",
-  "io.circe" %% "circe-parser"
-).map(_ % circeVersion)
+  "io.circe" %% "circe-core" % "0.14.3",
+  "io.circe" %% "circe-generic" % "0.14.3",
+  "io.circe" %% "circe-parser" % "0.14.3"
+)
--- src/main/scala/Classes/Notification.scala
+++ src/main/scala/Classes/Notification.scala
@@ -1,57 +1,77 @@
 package Classes
 
-//import javafx.scene.control.{DateCell, DatePicker}
-import scalafx.Includes._
-import scalafx.scene.control.{Button, DateCell, DatePicker}
-//import javafx.util.Callback
+import scalafx.Includes.*
+import scalafx.scene.control.Button
+import javafx.util.Callback
+
 import java.time.{LocalDate, Period}
 import scala.collection.mutable
+import com.calendarfx.view.CalendarView
+import javafx.collections.{FXCollections, ObservableList}
+import scalafx.Includes.*
+import javafx.scene.control.{DateCell, DatePicker, Tooltip}
+import scalafx.scene.Node
 
 case class Notification(name: String, publisher: User, pricePerDay: Double, pricePerHour: Double, description: String, category: Category, amount: Int, var available: Boolean):
 
-  val reservedDates = mutable.Buffer[Period]()
-  val datePicker = new DatePicker()
-  
-/*
-  // Set the day cell factory to customize cell appearance
-  datePicker.setDayCellFactory(new Callback[DatePicker, DateCell] {
+  val startAndEndDays = mutable.Buffer[(LocalDate, LocalDate)]()
+
+  val calendar: scalafx.scene.control.DatePicker = new DatePicker()
+  val cale: Node = new CalendarView()
+
+  def countDays(s: LocalDate, e: LocalDate): mutable.Buffer[LocalDate] =
+    val count = java.time.temporal.ChronoUnit.DAYS.between(s,e)
+    (0L to count).map(s.plusDays).toBuffer
+
+  def getRents =
+    WriteToFile().readRentsFromFile.filter( _.notification == this )
+
+  def reservedDays: mutable.Buffer[LocalDate] =
+    //not including start and end days
+    val rents = WriteToFile().readRentsFromFile.filter( _.notification == this )
+    val startDays = rents.map( _.startDay).toBuffer
+    val endDays = rents.map( _.endDay).toBuffer
+    val startWend = startDays.zip(endDays)
+    startWend.flatMap( i => countDays(i._1, i._2) ).filterNot( d => startDays.contains(d) || endDays.contains(d) )
+
+  def schedule =
+    val rents = WriteToFile().readRentsFromFile.filter( _.notification == this )
+    val startDays = rents.map( _.startDay).toBuffer
+    val endDays = rents.map( _.endDay).toBuffer
+    val startHours = rents.map( _.startHour).toBuffer
+    val endHours = rents.map( _.endHour).toBuffer
+    val days = startDays.zip(endDays)
+    val hours = startHours.zip(endHours)
+    days.zip(hours)
+
+
+  calendar.setDayCellFactory(new Callback[DatePicker, DateCell] {
     override def call(param: DatePicker): DateCell = new DateCell() {
       override def updateItem(item: LocalDate, empty: Boolean): Unit = {
         super.updateItem(item, empty)
 
-        // Check if the item (date) is within the rent period
-        if (!empty && item != null && isWithinRentPeriod(item)) {
-          // Set the background color of the cell to indicate it's within the rent period
-          style = ("-fx-background-color: pink")
+        if (!empty && item != null && ((schedule.exists( d => item.isEqual(d._1._1) && item.isEqual(d._1._2) )))) {
+          val i = schedule.indexWhere( d => item.isEqual(d._1._1) && item.isEqual(d._1._2))
+          setStyle("-fx-background-color: pink")
+          setTooltip(new Tooltip(s"Rented for: ${schedule(i)._2._1}-${schedule(i)._2._2}"))
         }
-      }
-
-      // Function to check if a date is within the rent period
-      private def isWithinRentPeriod(date: LocalDate): Boolean = {
-        reservedDates.contains(date)
-      }
-    }
-  })
-  
- */
-/*
-  datePicker.setDayCellFactory(new Callback[DatePicker, DateCell] {
-    override def call(param: javafx.scene.control.DatePicker): javafx.scene.control.DateCell = new javafx.scene.control.DateCell() {
-      override def updateItem(item: LocalDate, empty: Boolean): Unit = {
-        super.updateItem(item, empty)
-
-        if (!empty && item != null) {
-          if (reservedDates.contains(item)) {
-            setStyle("-fx-background-color: pink")
-          } else {
-            setStyle("-fx-background-color: green")
-          }
-        }
+        else if (!empty && item != null && ((schedule.exists( d => item.isEqual(d._1._1) )))) then
+          val i = schedule.indexWhere( d => item.isEqual(d._1._1) )
+          setStyle("-fx-background-color: pink")
+          setTooltip(new Tooltip(s"Rented from: ${schedule(i)._2._1} for the whole day"))
+        else if (!empty && item != null && ((schedule.exists( d => item.isEqual(d._1._2 )))) ) then
+          val i = schedule.indexWhere( d => item.isEqual(d._1._2) )
+          setStyle("-fx-background-color: pink")
+          setTooltip(new Tooltip(s"Rented until: ${schedule(i)._2._2}"))
+        else if (!empty && item != null && reservedDays.contains(item) ) then
+          setStyle("-fx-background-color: pink")
+          setTooltip(new Tooltip("Rented for the whole day"))
+        else
+          setTooltip(new Tooltip("Available"))
       }
     }
   })
 
- */
 
   val damages = mutable.Buffer[String]()
   var comments = mutable.Buffer[String]()
